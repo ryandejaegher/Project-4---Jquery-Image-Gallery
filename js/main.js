a139fa7faa85506fd909b862cf67fbf368179d76
Ryan Dejaegher <dejaegher.ryan@gmail.com>
2016-05-08 10:21:16 -0600

    Project 4 - Jquery Image Gallery

---
+++ js/main.js
@@ -0,0 +1,18 @@
+  $(document).ready(function(){
+    $(".search-nav").sticky({topSpacing:0});
+  });
+
+  $(document).ready(function(){
+  	$(lightbox.option)({
+      'showImageNumberLabel': false,
+      'wrapAround': true,
+      'maxHeight': 600,
+      'positionFromTop': 120,
+      'fitImagesInViewport': true
+    });
+
+ $(document).ready(function() {
+    $('#search').hideseek({
+    	attribute: 'alt'
+    });
+
