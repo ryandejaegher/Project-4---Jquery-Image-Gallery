5673e5b813970d4fa98f85d877e365a98ba95c23
Ryan Dejaegher <dejaegher.ryan@gmail.com>
2016-05-09 07:25:39 -0600

    Minor changes
    Cleaned up code and deleted scripts no longer being used.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@
     $(".search-nav").sticky({topSpacing:0});
   });
 
-  $(document).ready(function(){
-  	$(lightbox.option)({
+lightbox.option({
       'showImageNumberLabel': false,
       'wrapAround': true,
       'maxHeight': 600,
       'positionFromTop': 120,
       'fitImagesInViewport': true
-    });
+ })
 
  $(document).ready(function() {
     $('#search').hideseek({
